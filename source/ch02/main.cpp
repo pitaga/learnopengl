0bdfb8e19bb9565d59c25b3586a232379e1a961f
pitaga <jind_yang@hotmail.com>
2021-03-18 01:05:05 +0800

    shader compile check

--- source/ch02/main.cpp
+++ source/ch02/main.cpp
@@ -10,6 +10,9 @@ void init(GLFWwindow* window);
 void display(GLFWwindow* window, double currentTime);
 GLuint createShaderProgram();
 
+void printShaderLog(GLuint shader);
+void printProgramLog(int prog);
+bool checkOpenGLError();
 
 
 int main(void)
@@ -61,22 +64,85 @@ GLuint createShaderProgram()
 		"{ gl_Position = vec4(0.0, 0.0, 0.0, 1.0); }";
 
 	const char* fshaderSource = "#version 430 \n"
-		"out vec4 color \n"
+		"out vec4 color; \n"
 		"void main(void) \n"
-		"{ color = vec4(0.0, 0.0, 1.0, 1.0); }";
+		"{ if (gl_FragCoord.x < 300) color = vec4(0.0, 0.0, 1.0, 1.0); \n"
+		"  else color = vec4(1.0, 0.0, 0.0, 1.0); }";
+
 
 	GLuint vshader = glCreateShader(GL_VERTEX_SHADER);
-	GLuint fshader = glCreateShader(GL_FRAGMENT_SHADER);
-
 	glShaderSource(vshader, 1, &vshaderSource, NULL);
-	glShaderSource(fshader, 1, &fshaderSource, NULL);
 	glCompileShader(vshader);
+	GLint vertCompiled;
+	checkOpenGLError();
+	glGetShaderiv(vshader, GL_COMPILE_STATUS, &vertCompiled);
+	if (vertCompiled != 1) {
+		std::cout << "vertex compilation failed" << std::endl;
+		printShaderLog(vshader);
+	}
+
+	GLuint fshader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fshader, 1, &fshaderSource, NULL);
 	glCompileShader(fshader);
+	GLint fragCompiled;
+	checkOpenGLError();
+	glGetShaderiv(fshader, GL_COMPILE_STATUS, &fragCompiled);
+	if (fragCompiled != 1) {
+		std::cout << "fragment compilation failed" << std::endl;
+		printShaderLog(fshader);
+	}
 
 	GLuint program = glCreateProgram();
 	glAttachShader(program, vshader);
 	glAttachShader(program, fshader);
 	glLinkProgram(program);
+	GLint linked;
+	checkOpenGLError();
+	glGetProgramiv(program, GL_LINK_STATUS, &linked);
+	if (linked != 1) {
+		std::cout << "linking failed" << std::endl;
+		printShaderLog(program);
+	}
 
 	return program;
+}
+
+void printShaderLog(GLuint shader)
+{
+	int len = 0;
+	int chWrittn = 0;
+	char* log;
+	glGetShaderiv(shader, GL_INFO_LOG_LENGTH, &len);
+	if (len > 0) {
+		log = (char*)malloc(len);
+		glGetShaderInfoLog(shader, len, &chWrittn, log);
+		std::cout << "Shader Info Log: " << log << std::endl;
+		free(log);
+	}
+}
+
+void printProgramLog(int prog)
+{
+	int len = 0;
+	int chWrittn = 0;
+	char* log;
+	glGetProgramiv(prog, GL_INFO_LOG_LENGTH, &len);
+	if (len > 0) {
+		log = (char*)malloc(len);
+		glGetProgramInfoLog(prog, len, &chWrittn, log);
+		std::cout << "Program Info Log: " << log << std::endl;
+		free(log);
+	}
+}
+
+bool checkOpenGLError()
+{
+	bool foundError = false;
+	int glErr = glGetError();
+	while (glErr != GL_NO_ERROR) {
+		std::cout << "glError: " << glErr << std::endl;
+		foundError = true;
+		glErr = glGetError();
+	}
+	return foundError;
 }
